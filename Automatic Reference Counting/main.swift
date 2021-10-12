db04fd93e6f0a842325e1d7a1b3a1944005d61c6
Temich93s <nil>
2021-10-13 09:48:40 +1000

    Пройдена тема: ARC в действии

--- Automatic Reference Counting/main.swift
+++ Automatic Reference Counting/main.swift
@@ -9,3 +9,33 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: ARC в действии
+print("\n//ARC в действии")
+
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialized")
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
+
+reference1 = Person(name: "John Appleseed")
+// Prints "John Appleseed is being initialized"
+
+reference2 = reference1
+reference3 = reference1
+
+reference1 = nil
+reference2 = nil
+
+reference3 = nil
+// Prints "John Appleseed is being deinitialized"
